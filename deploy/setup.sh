845a1b5f4a4560a21602ef7722b6c4f7928fd25d
sanjithhithub <msanjith130@gmail.com>
2025-01-17 12:00:57 +0530

    adding the setup file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,10 +10,7 @@ PROJECT_BASE_PATH='/usr/local/apps/coconut_app'
 # Update system and install dependencies
 echo "Updating system and installing dependencies..."
 apt-get update -y && apt-get upgrade -y
-
-# Install required packages without version locks
-echo "Installing necessary packages..."
-apt-get install -y python3 python3-dev python3-venv sqlite3 libsqlite3-dev python3-pip supervisor nginx git locales
+apt-get install -y python3-dev python3-venv sqlite3 libsqlite3-dev python3-pip supervisor nginx git locales
 
 # Set Ubuntu locale (for language settings)
 echo "Setting system locale..."
@@ -40,7 +37,7 @@ python3 -m venv "$PROJECT_BASE_PATH/env"
 # Install Python dependencies
 echo "Installing Python dependencies..."
 "$PROJECT_BASE_PATH/env/bin/pip" install --upgrade pip
-"$PROJECT_BASE_PATH/env/bin/pip" install -r "$PROJECT_BASE_PATH/requirements.txt" uwsgi
+"$PROJECT_BASE_PATH/env/bin/pip" install -r "$PROJECT_BASE_PATH/requirements.txt" uwsgi==2.0.21
 
 # Run database migrations
 echo "Running database migrations..."
