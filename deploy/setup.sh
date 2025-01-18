03dde803363ad627867f2923e6f8598802dddd66
sanjithhithub <msanjith130@gmail.com>
2025-01-18 16:31:15 +0530

    changes made in setup file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,28 +10,31 @@ PROJECT_BASE_PATH='/usr/local/apps/coconut_app'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
+# Install Python, SQLite, and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
+# Create project directory and clone the repository
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
+# Create a Python virtual environment
 python3 -m venv $PROJECT_BASE_PATH/env
 
+# Install Python dependencies
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
 
-# Run migrations
+# Run database migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Setup Supervisor to run our uwsgi process.
+# Setup Supervisor to run the uWSGI process
 cp $PROJECT_BASE_PATH/deploy/supervisor_coconut_calculation.conf /etc/supervisor/conf.d/coconut_calculation.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart coconut_calculation
 
-# Setup nginx to make our application accessible.
+# Setup Nginx to make the application accessible
 cp $PROJECT_BASE_PATH/deploy/nginx_coconut_calculation.conf /etc/nginx/sites-available/coconut_calculation.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/coconut_calculation.conf /etc/nginx/sites-enabled/coconut_calculation.conf
